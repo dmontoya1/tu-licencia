7bd7a853b49981f59cdc3d4af8288062f0c24411
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-09-13 10:20:33 -0500

    Updating base settings

--- tulicencia/settings/base.py
+++ tulicencia/settings/base.py
@@ -37,7 +37,8 @@ PROJECT_APPS = [
     'manager',
     # 'reports',
     'users',
-    'vehicles'
+    'vehicles',
+    'webclient'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
