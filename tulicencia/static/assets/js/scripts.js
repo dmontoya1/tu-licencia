bd322a588e8ccf47c3ab5b386a0d3af50c3250a5
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-10-04 11:39:54 -0500

    Updating Step 1 and step 2 Validations

--- tulicencia/static/assets/js/scripts.js
+++ tulicencia/static/assets/js/scripts.js
@@ -24,6 +24,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
+
     $.backstretch("/static/images/fondo.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
@@ -42,27 +43,155 @@ jQuery(document).ready(function() {
     	$(this).removeClass('input-error');
     });
     
-    // next step
-    $('.f1 .btn-next').on('click', function() {
+    // next step 1
+    $('.f1 .btn-step-1').on('click', function() {
     	var parent_fieldset = $(this).parents('fieldset');
     	var next_step = true;
     	// navigation steps / progress steps
     	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
     	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
-    	
-    	// fields validation
-    	// parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
-    	// 	if( $(this).val() == "" ) {
-		// 		$(this).addClass('input-error');
-    	// 		next_step = false;
-    	// 	}
-    	// 	else {
-    	// 		$(this).removeClass('input-error');
-    	// 	}
-    	// });
-		// fields validation
-		next_step = true // Borrar este linea 
+		
+
+		
+		/* 
+		if ($('#cities').val() == "" || $('#cities').val() == null){
+			next_step = false;
+			swal({
+				title: 'Atención',
+				text: 'Para continuar debes seleccionar tu ubicación (Departamento y ciudad)',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			})
+		}
+
+		if (age == ""){
+			next_step = false;
+			swal({
+				title: 'Atención',
+				text: 'Para continuar debes seleccionar tu fecha de nacimiento',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			}).then((result) => {
+				if (result.value) {
+					$('html, body, #licence-request-form').animate({
+						scrollTop: $('#birthdate-select').offset().top
+					}, 1000);
+				}
+			})
+		}
+
+		if (gender == ""){
+			next_step = false;			
+			swal({
+				title: 'Atención',
+				text: 'Para continuar debes seleccionar tu género',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			  }).then((result) => {
+				if (result.value) {
+					scroll_to_class( $('.choice-gender'), 20 );
+				}
+			  })
+
+		}
+		*/
+
     	if( next_step ) {
+    		parent_fieldset.fadeOut(400, function() {
+    			// change icons
+    			current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+    			// progress bar
+    			bar_progress(progress_line, 'right');
+    			// show next step
+	    		$(this).next().fadeIn();
+	    		// scroll window to beginning of the form
+    			scroll_to_class( $('.f1'), 20 );
+	    	});
+    	}
+    	
+	});
+	
+	// next step 2
+    $('.f1 .btn-step-2').on('click', function() {
+    	var parent_fieldset = $(this).parents('fieldset');
+    	var next_step = true;
+    	// navigation steps / progress steps
+    	var current_active_step = $(this).parents('.f1').find('.f1-step.active');
+    	var progress_line = $(this).parents('.f1').find('.f1-progress-line');
+
+		if (runt == ""){
+			next_step = false;
+			swal({
+				title: 'Atención',
+				text: 'Para continuar debes seleccionar si estas inscrito en el RUNT',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			}).then((result) => {
+				if (result.value) {
+					$('html, body, #licence-request-form').animate({
+						scrollTop: $('#birthdate-select').offset().top
+					}, 1000);
+				}
+			})
+		}
+
+		if (car){
+			if (!('car' in licences)){
+				next_step = false;			
+				swal({
+					title: 'Atención',
+					text: 'Debes seleccionar la licencia para carro que vas a tramitar',
+					type: 'error',
+					showCancelButton: false,
+					confirmButtonText: 'Ok'
+				})
+			}
+		}
+		if (bike){
+			if (!('bike' in licences)){
+				next_step = false;			
+				swal({
+					title: 'Atención',
+					text: 'Debes seleccionar la licencia para moto que vas a tramitar',
+					type: 'error',
+					showCancelButton: false,
+					confirmButtonText: 'Ok'
+				})
+			}
+		}
+		if (!(bike) && !(car)){
+			next_step = false;			
+			swal({
+				title: 'Atención',
+				text: 'Debes seleccionar el tipo de licencia que vas a tramitar',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			})
+		}
+		if (tramit == ""){
+			next_step = false;
+			swal({
+				title: 'Atención',
+				text: 'Para continuar debes seleccionar el trámite que deseas realizar',
+				type: 'error',
+				showCancelButton: false,
+				confirmButtonText: 'Ok'
+			}).then((result) => {
+				if (result.value) {
+					$('html, body, #licence-request-form').animate({
+						scrollTop: $('#birthdate-select').offset().top
+					}, 1000);
+				}
+			})
+		}
+
+    	if( next_step ) {
+			
     		parent_fieldset.fadeOut(400, function() {
     			// change icons
     			current_active_step.removeClass('active').addClass('activated').next().addClass('active');
@@ -96,7 +225,5 @@ jQuery(document).ready(function() {
     });
     
     // submit
-	
-    
     
 });
