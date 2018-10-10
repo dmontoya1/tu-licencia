27e22fe0d41630fda4b56eff32a32ad1ba108efa
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-10-10 16:44:24 -0500

    Updating Sector Filters

--- tulicencia/static/js/admin/utils_admin.js
+++ tulicencia/static/js/admin/utils_admin.js
@@ -31,6 +31,37 @@
         });
     }
 
+    function getSectors(idCity) {
+        $.ajax({
+            type: "GET",
+            url:"/api/manager/sector/"+idCity+"/",
+            success:function(data)
+            {   
+                sector = $('select#id_sector option:selected').val()
+                $('select#id_sector').empty()
+                $(data).each(function(i, v){
+                    if (sector === v.id){
+                        $('select#id_sector').append(
+                            `<option selected value="${v.id}">${v.name}</option>`
+                        )
+                    }
+                    else{
+                        $('select#id_sector').append(
+                            `<option value="${v.id}">${v.name}</option>`
+                        )
+                    }
+                    
+                })
+                $('select#id_sector').prepend(
+                    `<option value="" disabled selected>Seleccione una ciudad</option>`
+                )
+                $('select#id_sector').val(sector).trigger('change');
+                $('select#id_sector').select2("destroy");
+                $('select#id_sector').select2();
+            },
+        });
+    }
+
     $(document).ready(function(){
         if ($('select#id_state option:selected').val()){
             getCities($('select#id_state option:selected').val())
@@ -41,9 +72,21 @@
                 '<option disabled value selected>Seleccione primero un departamento</option>'
             )
         }
+        if ($('select#id_city option:selected').val()){
+            getSectors($('select#id_city option:selected').val())
+        }
+        else {
+            $('select#id_sector').empty()
+            $('select#id_sector').append(
+                '<option disabled value selected>Seleccione primero una ciudad</option>'
+            )
+        }
 
         $('select#id_state').on('change', function(ev){
             getCities($(this).val())
+        })
+        $('select#id_city').on('change', function(ev){
+            getSectors($(this).val())
         })
     });
 
