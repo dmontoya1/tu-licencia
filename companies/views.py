e804023bc169d58c48a014a2c29d92958e944364
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-11-01 17:45:01 -0500

    Updating filters and validations

--- companies/views.py
+++ companies/views.py
@@ -24,8 +24,49 @@ class CrcList(generics.ListAPIView):
         get_list.pop('age')
         get_list.pop('gender')
         get_list.pop('licences')
+        try:
+            rating = get_list['rating']
+            get_list.pop('rating')
+        except:
+            rating = None
+        try:
+            price = get_list['price']
+            get_list.pop('price')
+        except:
+            price = None
+
         params = params_to_filter(get_list.items())
-        return Crc.objects.filter(**params).distinct()
+        query = Crc.objects.filter(**params).distinct()
+        try:
+            if rating:
+                if rating == "1":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('rating', 'price')
+                        elif price == "2":
+                            query = query.order_by('rating', '-price')
+                        else:
+                            query = query.order_by('rating')
+                    else:
+                        query = query.order_by('rating')
+                elif rating == "2":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('-rating', 'price')
+                        elif price == "2":
+                            query = query.order_by('-rating', '-price')
+                        else:
+                            query = query.order_by('-rating')
+                    else:
+                        query = query.order_by('-rating')
+            elif price:
+                if price == "1":
+                    query = query.order_by('price')
+                elif price == "2":
+                    query = query.order_by('-price')
+        except Exception as e:
+            print (e)
+        return query
 
 
 class CeaList(generics.ListAPIView):
@@ -39,8 +80,48 @@ class CeaList(generics.ListAPIView):
         get_list.pop('age')
         get_list.pop('gender')
         get_list.pop('licences')
+        try:
+            rating = get_list['rating']
+            get_list.pop('rating')
+        except:
+            rating = None
+        try:
+            price = get_list['price']
+            get_list.pop('price')
+        except:
+            price = None
         params = params_to_filter(get_list.items())
-        return Cea.objects.filter(**params).distinct()
+        query = Cea.objects.filter(**params).distinct()
+        try:
+            if rating:
+                if rating == "1":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('rating', 'final_price')
+                        elif price == "2":
+                            query = query.order_by('rating', '-final_price')
+                        else:
+                            query = query.order_by('rating')
+                    else:
+                        query = query.order_by('rating')
+                elif rating == "2":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('-rating', 'final_price')
+                        elif price == "2":
+                            query = query.order_by('-rating', '-final_price')
+                        else:
+                            query = query.order_by('-rating')
+                    else:
+                        query = query.order_by('-rating')
+            elif price:
+                if price == "1":
+                    query = query.order_by('final_price')
+                elif price == "2":
+                    query = query.order_by('-final_price')
+        except Exception as e:
+            print (e)
+        return query
 
 
 class TransitList(generics.ListAPIView):
@@ -50,8 +131,49 @@ class TransitList(generics.ListAPIView):
     serializer_class = TransitSerializer
 
     def get_queryset(self):
-        params = params_to_filter(self.request.GET.items())
-        return TransitDepartment.objects.filter(**params).distinct()
+        get_list = self.request.GET.copy()
+        try:
+            rating = get_list['rating']
+            get_list.pop('rating')
+        except:
+            rating = None
+        try:
+            price = get_list['price']
+            get_list.pop('price')
+        except:
+            price = None
+        params = params_to_filter(get_list.items())
+        query = TransitDepartment.objects.filter(**params).distinct()
+        try:
+            if rating:
+                if rating == "1":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('rating', 'price')
+                        elif price == "2":
+                            query = query.order_by('rating', '-price')
+                        else:
+                            query = query.order_by('rating')
+                    else:
+                        query = query.order_by('rating')
+                elif rating == "2":
+                    if price:
+                        if price == "1":
+                            query = query.order_by('-rating', 'price')
+                        elif price == "2":
+                            query = query.order_by('-rating', '-price')
+                        else:
+                            query = query.order_by('-rating')
+                    else:
+                        query = query.order_by('-rating')
+            elif price:
+                if price == "1":
+                    query = query.order_by('price')
+                elif price == "2":
+                    query = query.order_by('-price')
+        except Exception as e:
+            print (e)
+        return query
 
 
 class DisableCrcRequest(APIView):
