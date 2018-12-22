fae8604109a3badbe229c42d8c5ba3149439a0c2
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-12-22 10:08:21 -0500

    updating db pass

--- tulicencia/settings/prod.py
+++ tulicencia/settings/prod.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tulicencia',
         'USER': 'tulicencia',
-        'PASSWORD': 'licencia-pass',
+        'PASSWORD': 'tulicencia-pass',
         'HOST': 'localhost',
         'PORT': '',
     }
