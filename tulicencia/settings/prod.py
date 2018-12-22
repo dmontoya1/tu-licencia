f13054fd788210281c966c4b50a774e80ccce1d1
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-12-22 09:48:46 -0500

    Updating prod settings

---
+++ tulicencia/settings/prod.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['http://3.16.156.133/', '3.16.156.133']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tulicencia',
+        'USER': 'tulicencia',
+        'PASSWORD': 'licencia-pass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+STATIC_ROOT = '/var/www/tulicencia/static/'
+MEDIA_ROOT = '/var/www/tulicencia/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
+
+
+MEDIA_ROOT = 'media'
+
+EMAIL_ADMIN = 'tulicencia.apptitud@gmail.com'
