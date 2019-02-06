4c9de4d8048281cd4adc26931b3090441dea5b8d
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2019-02-06 09:14:13 -0500

    Updating fix

--- companies/signals.py
+++ companies/signals.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 import json
 import requests
-from math import sin, cos, sqrt, atan2, radians
 
 from rest_framework.exceptions import NotFound, APIException
 
@@ -15,84 +14,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 
-from .models import Cea, Crc, TransitDepartment, CeaRating, CrcRating, TransitRating
+from .models import CeaRating, CrcRating, TransitRating
 
-@receiver(post_save, sender=Cea)
-def update_address_cea(sender, **kwargs):
-    instance = kwargs.get('instance')
-    address = instance.address.replace(" ", "+")
-    address = address.replace("#", "+")
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address={},+{}&key={}".format(
-        address, instance.city.name, settings.GOOGLE_MAPS_API_KEY
-    )
-    result = requests.get(url)
-    my_json = result.content.decode('utf8').replace("'", '"')
-
-    data = json.loads(my_json)
-    try:
-        obj = data['results'][0]['geometry']['location']
-    except (KeyError, IndexError):
-        print ('Ha ocurrido un error al obtener latitud y longitud')
-        # messages.add_message(request, messages.WARNING, ('Ha ocurrido un error al obtener la latitud y la longitud'))
-        # raise APIException(detail=("Ha ocurrido un error. Por favor, vuelve a intentarlo"))
-
-    if data['status'] == 'OK':
-        print (obj)
-        if instance.lat == None or instance.lon == None:
-            instance.lat = obj['lat']
-            instance.lon = obj['lng']
-            instance.save()
-
-@receiver(post_save, sender=Crc)
-def update_address_crc(sender, **kwargs):
-
-    instance = kwargs.get('instance')
-    address = instance.address.replace(" ", "+")
-    address = address.replace("#", "+")
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address={},+{}&key={}".format(
-        address, instance.city.name, settings.GOOGLE_MAPS_API_KEY
-    )
-    result = requests.get(url)
-    my_json = result.content.decode('utf8').replace("'", '"')
-
-    data = json.loads(my_json)
-    try:
-        obj = data['results'][0]['geometry']['location']
-    except (KeyError, IndexError):
-        print ('Ha ocurrido un error al obtener latitud y longitud')
-        # messages.add_message(request, messages.WARNING, ('Ha ocurrido un error al obtener la latitud y la longitud'))
-        # raise APIException(detail=("Ha ocurrido un error. Por favor, vuelve a intentarlo"))
-
-    if data['status'] == 'OK':
-        if instance.lat == None or instance.lon == None:
-            instance.lat = obj['lat']
-            instance.lon = obj['lng']
-            instance.save()
-
-@receiver(post_save, sender=TransitDepartment)
-def update_address_transit(sender, **kwargs):
-    instance = kwargs.get('instance')
-    address = instance.address.replace(" ", "+")
-    address = address.replace("#", "+")
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address={},+{}&key={}".format(
-        address, instance.city.name, settings.GOOGLE_MAPS_API_KEY
-    )
-    result = requests.get(url)
-    my_json = result.content.decode('utf8').replace("'", '"')
-
-    data = json.loads(my_json)
-    try:
-        obj = data['results'][0]['geometry']['location']
-    except (KeyError, IndexError):
-        print ('Ha ocurrido un error al obtener latitud y longitud')
-        # messages.add_message(request, messages.WARNING, ('Ha ocurrido un error al obtener la latitud y la longitud'))
-        # raise APIException(detail=("Ha ocurrido un error. Por favor, vuelve a intentarlo"))
-
-    if data['status'] == 'OK':
-        if instance.lat == None or instance.lon == None:
-            instance.lat = obj['lat']
-            instance.lon = obj['lng']
-            instance.save()
 
 @receiver(post_save, sender=CeaRating)
 def update_rating_cea(sender, **kwargs):
